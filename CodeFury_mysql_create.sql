a62519625db95986c6ccb04d4373fba9515ea1ee
Hrushabh Hirudkar <44575079+HrushabhHirudkar@users.noreply.github.com>
2020-10-07 20:37:07 +0530

    SQL Create Tables and Other Queries

--- CodeFury_mysql_create.sql
+++ CodeFury_mysql_create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `meeting_room` (
-	`uid` varchar(20) NOT NULL AUTO_INCREMENT,
+	`uid` varchar(20) NOT NULL,
 	`capacity` INT NOT NULL,
 	`rating` INT NOT NULL,
 	`perhour_cost` INT NOT NULL,
@@ -12,8 +12,8 @@ CREATE TABLE `amenities` (
 );
 
 CREATE TABLE `users` (
-	`uid` varchar(20) NOT NULL AUTO_INCREMENT,
-	`name` varchar(20) NOT NULL,
+	`uid` varchar(20) NOT NULL,
+	`Membername` varchar(20) NOT NULL,
 	`email` varchar(20) NOT NULL,
 	`phone` varchar(20) NOT NULL,
 	PRIMARY KEY (`uid`)
@@ -21,14 +21,14 @@ CREATE TABLE `users` (
 
 CREATE TABLE `user_role` (
 	`uid` varchar(20) NOT NULL,
-	`role` varchar(20) NOT NULL,
+	`role_member` varchar(20) NOT NULL,
 	`credit` INT NOT NULL
 );
 
 CREATE TABLE `booking_info` (
-	`uid` varchar(20) NOT NULL AUTO_INCREMENT,
+	`uid` varchar(20) NOT NULL,
 	`room_no` varchar(20) NOT NULL,
-	`date` DATE NOT NULL,
+	`dateOfBooking` DATE NOT NULL,
 	`start_time` DATETIME NOT NULL,
 	`end_time` DATETIME NOT NULL,
 	`booked_by` varchar(20) NOT NULL,
@@ -36,10 +36,10 @@ CREATE TABLE `booking_info` (
 );
 
 CREATE TABLE `meeting` (
-	`uid` varchar(20) NOT NULL AUTO_INCREMENT,
+	`uid` varchar(20) NOT NULL,
 	`title` varchar(20) NOT NULL,
 	`booking_info` varchar(20) NOT NULL,
-	`type` varchar(20) NOT NULL,
+	`typeOfMeeting` varchar(20) NOT NULL,
 	PRIMARY KEY (`uid`)
 );
 
@@ -61,4 +61,3 @@ ALTER TABLE `meeting` ADD CONSTRAINT `meeting_fk0` FOREIGN KEY (`booking_info`) 
 ALTER TABLE `list_of_people` ADD CONSTRAINT `list_of_people_fk0` FOREIGN KEY (`uid`) REFERENCES `meeting`(`uid`);
 
 ALTER TABLE `list_of_people` ADD CONSTRAINT `list_of_people_fk1` FOREIGN KEY (`user_uid`) REFERENCES `users`(`uid`);
-
