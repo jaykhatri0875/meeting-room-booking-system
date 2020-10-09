560e3f2c7bc922a72724af22c9f5e02685b6c727
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2020-10-09 16:35:41 +0530

    Connect Meeting DAO with Service

--- src/com/hsbc/meetopia/service/MeetingService.java
+++ src/com/hsbc/meetopia/service/MeetingService.java
@@ -1,6 +1,8 @@
 package com.hsbc.meetopia.service;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import com.hsbc.meetopia.dao.MeetingDAO;
 import com.hsbc.meetopia.model.Meeting;
@@ -18,11 +20,17 @@ public class MeetingService {
 	}
 
 	public Meeting fetchMeetingByMeetingId(String meetingId) {
-		return meetingDAO.fetchMeetingByUID(meetingId);
+		return meetingDAO.fetchMeetingByMeetingId(meetingId);
 	}
 
-	public Meeting updateMeeting(String meetingId, Meeting meeting) {
-		return meetingDAO.updateMeeting(meetingId, meeting);
+	public Collection<Meeting> fetchMeetingsByUserId(String userId) {
+		List<Meeting> meetings = new ArrayList<>();
+		Collection<String> meetingIds = this.meetingDAO.fetchMeetingsByUserId(userId);
+		for (String meetingId : meetingIds) {
+			Meeting meeting = fetchMeetingByMeetingId(meetingId);
+			meetings.add(meeting);
+		}
+		return meetings;
 	}
 
 }
