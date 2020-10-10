c10d3efb28969d63e0241be64631e30925f57906
Shivani Rajmane <shivani.rajmane123@gmail.com>
2020-10-10 17:32:46 +0530

    Changes

--- src/com/hsbc/meetopia/util/DatabaseConnection.java
+++ src/com/hsbc/meetopia/util/DatabaseConnection.java
@@ -17,5 +17,15 @@ public class DatabaseConnection {
         } catch (ClassNotFoundException ex) {} catch (SQLException ex) {}
         return con;
     }
-
+    
+	/*
+	 * private static final String DB_URL =
+	 * "jdbc:mysql://db4free.net:3306/codefury"; private static final String USER =
+	 * "codefury"; private static final String PASS = "codefury";
+	 * 
+	 * public static Connection getRemoteConnection() { try {
+	 * Class.forName(JDBC_DRIVER); con = DriverManager.getConnection(DB_URL, USER,
+	 * PASS); return con; } catch (Exception e) { e.printStackTrace(); } return
+	 * null; }
+	 */
 }
